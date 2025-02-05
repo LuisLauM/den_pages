e4eccfc086812884fb19080c40a44e507b7da0bd
Wencheng Lau-Medrano <luis.laum@gmail.com>
2025-02-05 11:43:04 +0100

    Config changes

--- render_and_serve.sh
+++ render_and_serve.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm resources public -r
-Rscript R/mdGenerator.R --vanilla
+# Rscript R/mdGenerator.R --vanilla
 quarto render 
 hugo --cleanDestinationDir --gc
 hugo server --disableFastRender
